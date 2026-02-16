c7044ae7ae35eb5a89c73b8ed0efa117c95be079
fenreitsu <140250786+fenreitsu@users.noreply.github.com>
2026-02-16 21:11:00 +0100

    C42
    Actualizacion de todas las carpetas

--- C_Piscine/C_04/ex03/ft_atoi.c
+++ C_Piscine/C_04/ex03/ft_atoi.c
@@ -5,51 +5,80 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: reiascan <reiascan@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/13 01:21:21 by reiascan          #+#    #+#             */
-/*   Updated: 2026/02/13 12:42:44 by reiascan         ###   ########.fr       */
+/*   Created: 2026/02/12 09:43:28 by reiascan          #+#    #+#             */
+/*   Updated: 2026/02/16 21:08:39 by reiascan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-    int i;
-    int sign;
-    int result;
+	int	i;
+	int	sign;
+	int	result;
 
-    i = 0;
-    sign = 1;
-    result = 0;
-    while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    while (str[i] == '+' || str[i] == '-')
-    {
-        if (str[i] == '-')
-            sign *= -1;
-        i++;
-    }
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = (result * 10) + (str[i] - '0');
-        i++;
-    }
-    return (result * sign);
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
 
-#include <stdio.h>
+/* #include <stdio.h>
+#include <unistd.h>
 
-int ft_atoi(char *str);
-
-int main(int argc, char **argv)
+void	ft_putnbr(int nb)
 {
-    // Verificamos que el usuario haya pasado exactamente un argumento
-    if (argc == 2)
-    {
+	char	digit;
 
-        printf("%d\n", ft_atoi(argv[1]));
-    }
-    else
-    {
-        printf("Use: Program + \"string\"\n");
-    }
-    return (0);
+	if (nb == -2147483648)
+		write (1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		write (1, "-", 1);
+		ft_putnbr(nb * -1);
+	}
+	else
+	{
+		digit = (nb % 10) + '0';
+		if ((nb / 10) != 0)
+			ft_putnbr (nb / 10);
+		write (1, &digit, 1);
+	}
 }
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		ft_putnbr(ft_atoi(argv[1]));
+	else
+		ft_putstr("El uso correcto es \"[PROGRAMA]\" \"[STRING]\"");
+} */
+
+/* int	main(void)
+{
+	ft_putnbr(ft_atoi(" -+-a42"));
+} */
